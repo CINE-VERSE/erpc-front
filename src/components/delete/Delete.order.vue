7b10b2888e00cbf46c18f127f4caeded582a258c
dongjunsong <papi64129955@gmail.com>
2024-06-13 16:48:25 +0900

    css 살짝 수정

--- src/components/delete/Delete.order.vue
+++ src/components/delete/Delete.order.vue
@@ -27,7 +27,7 @@
             <table class="estimate2-table1">
                 <thead>
                     <tr>
-                        <th>수주서 코드</th>
+                        <th>프로젝트 코드</th>
                         <th>수주 금액</th>
                         <th>작성 일자</th>
                         <th>마감 일자</th>
